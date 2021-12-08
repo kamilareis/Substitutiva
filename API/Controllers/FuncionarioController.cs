02013d2f82d97ee43c9d2274d1c8d99befc2e37e
Kamilareis <34647680+kamilareis@users.noreply.github.com>
2021-12-08 19:30:31 -0300

    Primeiras configurações funcionario

---
+++ API/Controllers/FuncionarioController.cs
@@ -0,0 +1,19 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[api/funcionario")]
+    public class FuncionarioController : ControllerBase
+    {
+        //POST: api/funcionario/cadastrar
+        [HttpPost]
+        [Route("casadastro")]
+        public Funcionario Cadatrar(Funcionario funcionario)
+        {
+            funcionario.Nome += "cadastrado"; 
+            return funcionario;
+        }
+    }
+}
---
+++ API/Models/Funcionario.cs
@@ -0,0 +1,10 @@
+namespace API.Models
+{
+    public class Funcionario
+    {
+        public string Id {get; set;}
+        public string Cpf {get; set;}
+        public string Nome {get; set;}
+        public string CriadoEm {get; set;}        
+    }
+}
